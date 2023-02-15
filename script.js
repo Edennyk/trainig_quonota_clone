e6c114f22642272515516561daf7e7566e69d6c2
Eden <ekim@ubixeng.com>
2023-02-15 15:07:16 -0500

    changed classes name/ added swiper slide

--- script.js
+++ script.js
@@ -1,59 +1,112 @@
-// When scroll down, sticky Header show and hide 
-$(document).ready(function() {
-  $(".header-sticky").hide();
-  $(function() {
-      $(window).scroll(function() {
-          if ($(this).scrollTop() > 10) {
-              $('.header-sticky').fadeIn();
-              $('.toggle__menu span').css('background-color','#0E0E0E');
-          } else {
-              $('.header-sticky').fadeOut();
-              $('.toggle__menu span').css('background-color','#fff');
-          }
-      });
-  });
-
-});
-
-$(document).ready(function(){
-    $(".open").click(function(){
-     if($('.open').hasClass('open')){
-        $('.open').addClass('close').removeClass('open');
-        $('.mobile-nav').css('visibility', 'visible');
-     }else if($('.close').hasClass('close')){
-        $('.close').addClass('open').removeClass('close');
-        $('.mobile-nav').css('visibility', 'hidden');
-     }
-    });  
-  });
-
-  // img slider
+// When scroll down, sticky Header show and hide
+$(document).ready(function () {
+  $(".sticky").hide();
   $(function () {
-    $('.slider').slick({
-      slide: 'div', 
-        slidesToShow: 3,
-        arrows : true,  
-        prevArrow: $('.prev'),
-        nextArrow: $('.next'),
-    });
-  });
-
-// rolling number
-$(function(){
-
-
-
-  $(window).scroll(function(){
-    var scrollValue = $('.stat-section').offset().top;
-    
-  if($(window).scrollTop() >= scrollValue){
-    $({val:52}).animate({val:100}, {
-      duration:2000,
-      progress:function(){
-        var now = this.val;
-        $('.count_num').text(Math.ceil(now));
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 10) {
+        $(".sticky").fadeIn();
+        $("#toggle__menu span").css("background-color", "#0E0E0E");
+      } else {
+        $(".sticky").fadeOut();
+        $("#toggle__menu span").css("background-color", "#fff");
       }
     });
-  }
+  });
+});
+
+$(document).ready(function () {
+  $(".open").click(function () {
+    if ($(".open").hasClass("open")) {
+      $(".open").addClass("close").removeClass("open");
+      $(".mobile").css("visibility", "visible");
+    } else if ($(".close").hasClass("close")) {
+      $(".close").addClass("open").removeClass("close");
+      $(".mobile").css("visibility", "hidden");
+    }
+  });
+});
+
+// Home scroll down button
+$(".side__link").click(function (event) {
+  event.preventDefault();
+  $("html,body").animate({ scrollTop: $(this.hash).offset().top }, 500);
+});
+
+// Statistic section rolling number
+$(function () {
+  $(window).scroll(function () {
+    var scrollValue = $(".stat").offset().top - window.innerHeight;
+    if ($(window).scrollTop() >= scrollValue) {
+      $({ val: 52 }).animate(
+        { val: 100 },
+        {
+          duration: 2000,
+          progress: function () {
+            var now = this.val;
+            $(".first-num").text(Math.ceil(now));
+          },
+        }
+      );
+    }
+  });
+});
+
+$(function () {
+  $(window).scroll(function () {
+    var scrollValue = $(".stat").offset().top - window.innerHeight;
+    if ($(window).scrollTop() >= scrollValue) {
+      $({ val: 10 }).animate(
+        { val: 50 },
+        {
+          duration: 2000,
+          progress: function () {
+            var now = this.val;
+            $(".second-num").text(Math.ceil(now));
+          },
+        }
+      );
+    }
+  });
+});
+
+$(function () {
+  $(window).scroll(function () {
+    var scrollValue = $(".stat").offset().top - window.innerHeight;
+    if ($(window).scrollTop() >= scrollValue) {
+      $({ val: 1 }).animate(
+        { val: 4 },
+        {
+          duration: 2000,
+          progress: function () {
+            var now = this.val;
+            $(".last-num").text(Math.ceil(now));
+          },
+        }
+      );
+    }
+  });
+});
+
+$("document").ready(function () {
+  var swiper = new Swiper(".swiper-container", {
+    slidesPerView: "3",
+    spaceBetween: 80,
+    speed: 600,
+    direction: "horizontal",
+    keyboardControl: true,
+    mousewheelControl: "true",
+    loopedSlides: 7,
+
+    navigation: {
+      nextEl: ".slider__arrow--next",
+      prevEl: ".slider__arrow--prev",
+    },
+
+    breakpoints: {
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+    },
   });
 });
