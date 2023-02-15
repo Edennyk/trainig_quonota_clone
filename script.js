d04729da7e6262782fa0947ab035a91e0a0f1556
Eden <ekim@ubixeng.com>
2023-02-15 15:18:18 -0500

    edited script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// Home scroll down button
+$(".side__link").click(function (event) {
+  event.preventDefault();
+  $("html,body").animate({ scrollTop: $(this.hash).offset().top }, 500);
+});
+
 // When scroll down, sticky Header show and hide
 $(document).ready(function () {
   $(".sticky").hide();
@@ -26,66 +32,51 @@ $(document).ready(function () {
   });
 });
 
-// Home scroll down button
-$(".side__link").click(function (event) {
-  event.preventDefault();
-  $("html,body").animate({ scrollTop: $(this.hash).offset().top }, 500);
-});
-
 // Statistic section rolling number
-$(function () {
-  $(window).scroll(function () {
+function rollingNumber(){
+  $({ val: 0}).animate(
+    { val: 100 },
+    {
+      duration: 1500,
+      progress: function () {
+        var now = this.val;
+        $(".first-num").text(Math.ceil(now));
+      },
+    }
+  );
+
+  $({ val: 10 }).animate(
+    { val: 50 },
+    {
+      duration: 1500,
+      progress: function () {
+        var now = this.val;
+        $(".second-num").text(Math.ceil(now));
+      },
+    }
+  );
+
+  $({ val: 1 }).animate(
+    { val: 4 },
+    {
+      duration: 1500,
+      progress: function () {
+        var now = this.val;
+        $(".last-num").text(Math.ceil(now));
+      },
+    }
+  );
+}
+
+$(document).ready(function(){
+  $(window).scroll(function(){
     var scrollValue = $(".stat").offset().top - window.innerHeight;
     if ($(window).scrollTop() >= scrollValue) {
-      $({ val: 52 }).animate(
-        { val: 100 },
-        {
-          duration: 2000,
-          progress: function () {
-            var now = this.val;
-            $(".first-num").text(Math.ceil(now));
-          },
-        }
-      );
+      rollingNumber();
     }
   });
 });
 
-$(function () {
-  $(window).scroll(function () {
-    var scrollValue = $(".stat").offset().top - window.innerHeight;
-    if ($(window).scrollTop() >= scrollValue) {
-      $({ val: 10 }).animate(
-        { val: 50 },
-        {
-          duration: 2000,
-          progress: function () {
-            var now = this.val;
-            $(".second-num").text(Math.ceil(now));
-          },
-        }
-      );
-    }
-  });
-});
-
-$(function () {
-  $(window).scroll(function () {
-    var scrollValue = $(".stat").offset().top - window.innerHeight;
-    if ($(window).scrollTop() >= scrollValue) {
-      $({ val: 1 }).animate(
-        { val: 4 },
-        {
-          duration: 2000,
-          progress: function () {
-            var now = this.val;
-            $(".last-num").text(Math.ceil(now));
-          },
-        }
-      );
-    }
-  });
-});
 
 $("document").ready(function () {
   var swiper = new Swiper(".swiper-container", {
